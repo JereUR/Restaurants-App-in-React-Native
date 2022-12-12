74d15b29b63b647d90527f1c6e78ae4e5efb8817
Jeremias Dominguez Vega <jeremias.jdv@gmail.com>
2022-12-12 20:17:37 -0300

    Fix firebase config

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,4 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { firebaseConfig } from "./firebase-config";
+import { firebaseConfig } from "./firebase-config"; //Your firebase config
 
 export const app = initializeApp(firebaseConfig);
