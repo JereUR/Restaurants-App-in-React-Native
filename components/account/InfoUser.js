645c05ec1e5328cf2dcd264384d36aa4f16eec5c
Jeremias Dominguez Vega <jeremias.jdv@gmail.com>
2022-12-17 10:07:37 -0300

    User Screen

---
+++ components/account/InfoUser.js
@@ -0,0 +1,44 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Avatar } from "react-native-elements";
+
+export default function InfoUser({ user }) {
+  return (
+    <View style={styles.container}>
+      <Avatar
+        rounded
+        size="large"
+        source={
+          user.photoURL
+            ? { uri: user.photoURL }
+            : require("../../assets/avatar-default.jpg")
+        }
+      />
+      <View style={styles.infoUser}>
+        <Text style={styles.displayName}>
+          {user.displayName ? user.displayName : "Anonymous"}
+        </Text>
+        <Text>{user.email}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    backgroundColor: "#f9f9f9",
+    paddingVertical: 30,
+  },
+
+  infoUser: {
+    marginLeft: 20,
+  },
+
+  displayName: {
+    fontWeight: "bold",
+    paddingBottom: 5,
+  },
+});
